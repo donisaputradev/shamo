f228c8fda4326ded78b55d70cd1b353afcad6a9b
donisaputradev <donisaputradev@gmail.com>
2023-05-30 19:04:58 +0700

    🐞 fix: vite

--- resources/views/components/app_layout_auth.blade.php
+++ resources/views/components/app_layout_auth.blade.php
@@ -7,7 +7,7 @@
     <title>{{ $title }}</title>
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}" />
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script src="{{ asset('js/init-alpine.js') }}"></script>
